8278554727acfee72c8096bc1b162d6367691284
lipecalixto <fellipe.calixto.santos@gmail.com>
2020-06-03 18:03:12 -0300

    DropDown

--- models/Banco.js
+++ models/Banco.js
@@ -1,22 +1,22 @@
 const Sequelize=require('sequelize')
 
-/*
+
 const sequelize=new Sequelize('sistemadecadastro','root','1234',{
     host:"localhost",
     dialect:"mysql"
 })
+/*
+const sequelize=new Sequelize('sistemadecadastro','userrede','291623',{
+    host:"localhost",
+    dialect:"mysql"
+})
+
 
 const sequelize=new Sequelize('sistemadecadastro','userrede','291623',{
     host:"localhost",
     dialect:"mysql"
 })
 */
-
-const sequelize=new Sequelize('sistemadecadastro','userrede','291623',{
-    host:"localhost",
-    dialect:"mysql"
-})
-
 module.exports= {
     Sequelize: Sequelize,
     sequelize: sequelize
