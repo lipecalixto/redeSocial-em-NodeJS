b6fb6785c8d726751cff1a85b98fb63fe4eab5d8
leosilvaalves <leo_silvaalves@yahoo.com.br>
2020-06-03 20:16:14 -0300

    adicionado o banco de dados da aws: correcao do nome do usuario

--- models/Banco.js
+++ models/Banco.js
@@ -1,22 +1,11 @@
 const Sequelize=require('sequelize')
 
 
-const sequelize=new Sequelize('sistemadecadastro','root','1234',{
-    host:"localhost",
-    dialect:"mysql"
-})
-/*
-const sequelize=new Sequelize('sistemadecadastro','userrede','291623',{
-    host:"localhost",
+const sequelize=new Sequelize('bdederone','root','root291623',{
+    host:"database-aws.cidmehxsi5hh.us-east-1.rds.amazonaws.com",
     dialect:"mysql"
 })
 
-
-const sequelize=new Sequelize('sistemadecadastro','userrede','291623Leo',{
-    host:"mysql669.umbler.com",
-    dialect:"mysql"
-})
-*/
 module.exports= {
     Sequelize: Sequelize,
     sequelize: sequelize
