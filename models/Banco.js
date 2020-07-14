ab1ad0379b936de0a629ebfd7c75e3378ecd8bbe
lipecalixto <fellipe.calixto.santos@gmail.com>
2020-07-14 12:36:08 -0300

    Arrumando dropdown

--- models/Banco.js
+++ models/Banco.js
@@ -1,25 +1,25 @@
 const Sequelize=require('sequelize')
 
 
-/*
+
 const sequelize=new Sequelize('bdederone','root','root291623',{
     host:"database-aws.cidmehxsi5hh.us-east-1.rds.amazonaws.com",
     dialect:"mysql"
 })
-
+/*
 const sequelize=new Sequelize('sistemadecadastro','userrede','291623',{
     host:"localhost",
     dialect:"mysql"
 })
 
-*/
+
 
 
 const sequelize=new Sequelize('bdederone','root','root291623',{
     host:"database-aws.cidmehxsi5hh.us-east-1.rds.amazonaws.com",
     dialect:"mysql"
 })
-
+*/
 module.exports= {
     Sequelize: Sequelize,
     sequelize: sequelize
